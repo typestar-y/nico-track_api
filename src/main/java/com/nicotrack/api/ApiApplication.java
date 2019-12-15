d7a6eec1578db7d993047b519ac94ea7dcd342a6
typestar-y <typestar.y@gmail.com>
2019-12-15 15:58:22 +0900

    update: Firebase SDKを追加した

--- src/main/java/com/nicotrack/api/ApiApplication.java
+++ src/main/java/com/nicotrack/api/ApiApplication.java
@@ -1,13 +1,24 @@
 package com.nicotrack.api;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class ApiApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(ApiApplication.class, args);
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.getApplicationDefault())
+                .build();
+
+        FirebaseApp.initializeApp(options);
     }
 
 }
